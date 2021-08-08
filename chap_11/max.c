8f2c73fba53f4223f81abb1890f8eda99f207c12
Randy Kwalar <rkwalar@gmail.com>
2021-08-08 22:32:23 +0100

    Add max.c to chap_11

---
+++ chap_11/max.c
@@ -0,0 +1,27 @@
+/**
+ * @file
+ * @brief Finds the bigger element given 2 elements (numbers)
+ */
+
+#include <stdio.h>
+
+int *max(int *a, int *b);
+
+int main(void) {
+    int i, j;
+    
+    printf("Enter 2 numbers: ");
+    scanf("%d %d", &i, &j);
+
+    int *p = max(&i, &j);
+
+    printf("The bigger element is %d\n", *p);
+}
+
+int *max(int *a, int *b) {
+    if(*a > *b)
+        return a;
+    else
+        return b;
+}
+
