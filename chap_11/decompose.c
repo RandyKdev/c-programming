b2eb6909a814843452073d7ca265d298230fa243
Randy Kwalar <rkwalar@gmail.com>
2021-08-08 21:50:45 +0100

    Add chapter 11 and decompose program and gitignore file

---
+++ chap_11/decompose.c
@@ -0,0 +1,20 @@
+/**
+ * @file
+ * @brief Program separates a given number to its integer and fractional parts
+ */
+
+#include <stdio.h>
+
+void decompose(double x, long *int_part, double *frac_part);
+
+int main() {
+    long int_part;
+    double frac_part;
+    decompose(3.145893, &int_part, &frac_part);
+    printf("Int part = %ld \nFractional part = %f\n", int_part, frac_part);
+}
+
+void decompose(double x, long *int_part, double *frac_part) {
+    *int_part = (long) x;
+    *frac_part = x - *int_part;
+}
