3036f6546988dfdcbb633d2088078864fe88b555
Randy Kwalar <rkwalar@gmail.com>
2021-08-20 15:44:18 +0100

    Add exercise 11 of chap 12

---
+++ chap_12/exercises/11_find_largest.c
@@ -0,0 +1,25 @@
+#include <assert.h>
+
+#define N 10
+
+int *find_largest(int *a, int n) {
+    int *largest = a;
+    
+    for(n; n > 0; n--) {
+        if(*(++a) > *largest) {
+            largest = a;
+        }
+    }
+
+    return largest;
+}
+
+int main(void) {
+    int a[N] = {1, 2, 3, 6, 4, 5};
+    
+    int *largest = find_largest(a, N);
+
+    assert(largest == a + 3);
+
+    return 0;
+}
