c3d0cdd0be011ea1854f4e1437225a99969c987e
Randy Kwalar <rkwalar@gmail.com>
2021-08-23 23:56:57 +0100

    Add project 2b of chap 12

---
+++ chap_12/projects/2b_palindrome.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAX 100
+
+void getMessage(char *s);
+bool palindrome(char *s);
+
+int main(void) {
+    char message[MAX + 1];
+
+    getMessage(message);
+
+    bool isPalidrome = palindrome(message);
+
+    printf("%s\n", isPalidrome ? "Palindrome" : "Not a palindrome");
+
+    return 0;
+}
+
+void getMessage(char *s) {
+    printf("Enter a message: ");
+
+    int c;
+    char *i = s;
+
+    while((c = getchar()) != '\n' && i < s + MAX) {
+        if(!isalpha(c)) continue;
+        *i++ = tolower(c);
+    }
+    *i = '\0';
+}
+
+bool palindrome(char *s) {
+    int length = strlen(s);
+    for(char *i = s; i < s + (length / 2); i++) {
+        if(*i != *((s + length - 1) - (i - s))) 
+            return false;
+    }
+
+    return true;
+}
