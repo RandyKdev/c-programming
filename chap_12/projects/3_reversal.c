bd16f40247818c69f5152918c29382dd275c5e73
RandyKdev <rkwalar@gmail.com>
2021-08-25 18:03:13 +0100

    Add documentation in  project 3 of chap 12

--- chap_12/projects/3_reversal.c
+++ chap_12/projects/3_reversal.c
@@ -3,17 +3,20 @@
 #define LEN 50
 
 int main(void) {
-    char message[LEN];
+    char message[LEN]; // stores the message entered by the user
 
     printf("Enter a message: ");
 
-    int c; 
-    char *i = message; 
+    int c; // holds a character from stdin
+    char *i = message; // keeps track of position in `message`
 
+    // gets the user's message and stored it in `message`
     while((c=getchar()) != '\n' && i - message < LEN) {
+        // store the character read at the appropriate location in `message`
         *(i++) = c;
     }
     
+    // prints `message` in reversed order
     printf("Reversal is: ");
     for(i--; i >= message; i--) {
         printf("%c", *i);
