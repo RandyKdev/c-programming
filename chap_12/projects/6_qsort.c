8ae8e178f1875e9983382e4b5b5a1e6b8929b930
RandyKdev <rkwalar@gmail.com>
2021-08-27 23:50:57 +0100

    Add documentation to project 6 of chap 12

--- chap_12/projects/6_qsort.c
+++ chap_12/projects/6_qsort.c
@@ -6,13 +6,18 @@ void quicksort(int a[], int *low, int *high);
 int *split(int a[], int *low, int *high);
 
 int main(void) {
-    int a[N], i;
+    int a[N]; // holds numbers entered by user to be sorted
+    int i; // used as counter variable
 
+    // reads the numbers from stdin to array
     printf("Enter %d numbers to be sorted: ", N);
     for(i = 0; i < N; i++)
         scanf("%d", &a[i]);
+
+    // Sorts the array
     quicksort(a, a, a + N - 1);
 
+    // prints the contents of the array
     printf("In sorted order: ");
     for(i = 0; i < N; i++)
         printf("%d ", a[i]);
