e8699596521073515862f35e553d9bfee3764833
Randy Kwalar <rkwalar@gmail.com>
2021-09-08 00:20:11 +0100

    Add project 2c of chapter 13

---
+++ chap_13/projects/2c_remind.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+#include<string.h>
+
+#define MAX_REMIND 50 /* maximum number of reminders */
+#define MSG_LEN 60 /* max length of reminder message */
+
+int read_line(char str[], int n);
+
+int main(void) {
+    char reminders[MAX_REMIND][MSG_LEN + 11];
+    char day_str[3], time_str[7], msg_str[MSG_LEN + 1];
+    char combineDaysTime[9], month[4];
+    int day, i, j, num_remind = 0;
+
+    for(;;) {
+        if(num_remind == MAX_REMIND) {
+            printf("-- No space left --\n");
+            break;
+        }
+
+        printf("Enter month/day, 24 hour time and reminder: ");
+        
+        scanf("%3s ", month);
+        
+        scanf("%2d", &day);
+        if(day == 0) break;
+      
+        sprintf(day_str, "%2d", day);
+
+        fgets(time_str, 6, stdin);
+        
+        read_line(msg_str, MSG_LEN);
+
+        if(day < 0 || day > 31) continue;
+
+        strcat(strcpy(combineDaysTime, day_str), time_str);
+
+        for(i = 0; i < num_remind; i++) {
+            if(strcmp(combineDaysTime, reminders[i]) < 0)
+                break;
+        }
+
+        for(j = num_remind; j > i; j--)
+            strcpy(reminders[j], reminders[j-1]);
+        
+        strcpy(reminders[i], month);
+        strcpy(reminders[i], combineDaysTime);
+        strcat(reminders[i], msg_str);
+
+        num_remind++;
+    }
+
+    printf("\nDay Time  Reminder\n");
+    for(i = 0; i < num_remind; i++)
+        printf(" %s\n", reminders[i]);
+
+    return 0;
+}
+
+int read_line(char str[], int n) {
+    int ch, i = 0;
+
+    while((ch = getchar()) != '\n')
+        if(i < n)
+            str[i++] = ch;
+    str[i] = '\0';
+    return i;
+}
