63162eb8a8fc723afb7689a4ce11ff36013fe302
RandyKdev <rkwalar@gmail.com>
2021-09-19 12:47:27 +0100

    Modify exercise 11 of chapter 14 to accept variable number of arguments

--- chap_14/exercises/11_error.c
+++ chap_14/exercises/11_error.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
-#define ERROR(format_str, arg) fprintf(stderr, format_str, arg)
+#define ERROR(format_str, ...) (fprintf(stderr, (format_str), __VA_ARGS__))
 
 int main(void) {
     ERROR("Range error: index = %d\n", 3);
