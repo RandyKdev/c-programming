a98be6a08a83909735c5bbd74e2eb40624b62f50
RandyKdev <rkwalar@gmail.com>
2021-09-19 13:23:00 +0100

    Add documentation to exercise 8 of chapter 14

--- chap_14/exercises/8_line.c
+++ chap_14/exercises/8_line.c
@@ -2,7 +2,11 @@
 
 #define STR(x) #x
 #define STR1(x) STR(x)
-#define LINE_FILE "Line " STR1(__LINE__) " of file " __FILE__
+#define LINE_FILE ("Line " STR1(__LINE__) " of file " __FILE__)
+
+// calling STR1 and then STR works because we need the value of the macro
+// __LINE__ if we didnt, it would have converted __LINE__ to a string instead
+// which is not the functionality we want
 
 int main(void) {
     const char *str = LINE_FILE;
