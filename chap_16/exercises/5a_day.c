b5ed94b73802b82a43761c67a016a29188567ede
Randy Kwalar <rkwalar@gmail.com>
2021-10-18 20:15:43 +0100

    Modified function isLeapYear in exercise 5a of chapter 16 to include all cases

--- chap_16/exercises/5a_day.c
+++ chap_16/exercises/5a_day.c
@@ -5,9 +5,15 @@ struct date {
     int month, year, day;
 };
 
-// This function is not exhaustive
 bool isLeapYear(int year) {
-    if(year / 4 == 0) return true;
+    if(year % 4 == 0 && year % 100 != 0) {
+        return true;
+    }
+    
+    if(year % 100 == 0 && year % 400 == 0) {
+        return true;
+    }
+
     return false;
 }
 
