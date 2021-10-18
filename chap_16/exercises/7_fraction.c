7bfb62231680f059e17126e23321d746a6aa6c20
Randy Kwalar <rkwalar@gmail.com>
2021-10-18 21:21:23 +0100

    Done exercise 7c of chapter 16

--- chap_16/exercises/7_fraction.c
+++ chap_16/exercises/7_fraction.c
@@ -21,10 +21,24 @@ struct fraction add_fraction(struct fraction f1, struct fraction f2) {
 
     int n1 = f1.numerator * f2.denominator;
     int n2 = f2.numerator * f1.denominator;
-    
+
     struct fraction add;
     add.denominator = f1.denominator * f2.denominator;
     add.numerator = n1 + n2;
     add = reduce_fraction(add);
     return add;
+}
+
+struct fraction subtract_fraction(struct fraction f1, struct fraction f2) {
+    f1 = reduce_fraction(f1);
+    f2 = reduce_fraction(f2);
+
+    int n1 = f1.numerator * f2.denominator;
+    int n2 = f2.numerator * f1.denominator;
+    
+    struct fraction sub;
+    sub.denominator = f1.denominator * f2.denominator;
+    sub.numerator = n1 - n2;
+    sub = reduce_fraction(sub);
+    return sub;
 }
