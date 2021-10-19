8140b9bdbfb2689a980c42b05698a6e15ea0315c
Randy Kwalar <rkwalar@gmail.com>
2021-10-19 21:29:42 +0100

    Add exercise 9a of chapter 16

---
+++ chap_16/exercises/9_color.c
@@ -0,0 +1,16 @@
+struct color {
+    int red;
+    int green;
+    int blue;
+};
+
+struct color make_color(int red, int green, int blue) {
+    if(red < 0) red == 0;
+    if(green < 0) green == 0;
+    if(blue < 0) blue == 0;
+    if(red > 255) red == 255;
+    if(green > 255) green == 255;
+    if(blue > 255) blue == 255;
+
+    return (struct color) {.red = red, .green = green, .blue = blue};
+}
