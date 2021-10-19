31cdf47f0e1ee6ccbeb9881158297f201f6e5d02
Randy Kwalar <rkwalar@gmail.com>
2021-10-19 22:00:12 +0100

    Add exercise 10d of chapter 16

--- chap_16/exercises/10_rect.c
+++ chap_16/exercises/10_rect.c
@@ -1,5 +1,7 @@
 // NB: Considers that the plane is found in the positive x and y axis
 
+#include <stdbool.h>
+
 struct point {
     int x, y;
 };
@@ -33,4 +35,12 @@ struct rectangle move_rectangle(struct rectangle r, int x, int y) {
     r.upper_left.y += y;
     
     return r;
+}
+
+bool in_rectangle(struct rectangle r, struct point p) {
+    if(p.x >= r.upper_left.x && p.x <= r.lower_right.x && p.y >= r.lower_right.y && p.y <= r.upper_left.y) {
+        return true;
+    }
+
+    return false;
 }
