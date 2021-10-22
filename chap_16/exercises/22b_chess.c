d7fa879c5a5ec541696ed06d3073b71fedfa6fad
RandyKdev <rkwalar@gmail.com>
2021-10-22 04:35:54 +0100

    Add exercise 22b of chapter 16

---
+++ chap_16/exercises/22b_chess.c
@@ -0,0 +1,10 @@
+enum chess_pieces {KING, QUEEN, ROOK, BISHOP, KNIGHT, PAWN};
+
+int piece_value[] = {
+    [KING] = 200, 
+    [QUEEN] = 9, 
+    [ROOK] = 5, 
+    [BISHOP] = 3, 
+    [KNIGHT] = 3, 
+    [PAWN] = 1
+};
